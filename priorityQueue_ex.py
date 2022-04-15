71992375f72967ce07e3cfc90dd89d7dd0a35a03
Kihyun Lee <wezen@Kihyunui-MacBookAir.local>
2022-04-15 17:42:32 +0900

    heapq

--- priorityQueue_ex.py
+++ priorityQueue_ex.py
@@ -1,36 +1,27 @@
-from queue import PriorityQueue
 import heapq
 
 if __name__ == '__main__':
-    # 1. min_heap
+    # min_heap
     h = []
-    heapq.heappush(h, 4)
-    heapq.heappush(h, 5)
-    heapq.heappush(h, 1)
-    # [1, 5, 4]
-    heapq.heappop(h)  # 1
-    # [4, 5]
-
-
-    # 2. max_heap
-    temp = [10, 40, 50]
-    h = []
-    result = []
-    for i in temp:
-        heapq.heappush(h, -i)
+    heapq.heappush(h, (4, 'what'))
+    heapq.heappush(h, (3, ['d']))
+    heapq.heappush(h, (1, 4444))
+    heapq.heappush(h, (2, (11, 1)))
     while h:
-        result.append(-heapq.heappop(h))
-    print(result)
+        print(heapq.heappop(h))
+    print()
 
+    # max_heap
+    heapq.heappush(h, (-4, 'what'))
+    heapq.heappush(h, (-3, ['d']))
+    heapq.heappush(h, (-1, 4444))
+    heapq.heappush(h, (-2, (11, 1)))
+    while h:
+        print(heapq.heappop(h))
+    print()
 
-    # PriorityQueue
-    que = PriorityQueue()
-    que.put((4, 'middle'))
-    que.put((5, 'five'))
-    que.put((1, 'first'))
-    que.put((8, 'low'))
-    # [(1, 'first'), (4, 'middle'), (5, 'five'), (8, 'low')]
-    print(que.get())
-    print(que.get())
-    print(que.qsize())
-    print(que.empty())
+    # heapify
+    h = [(4, 'what'), (3, ['d']), (1, 4444), (2, (11, 1))]
+    heapq.heapify(h)
+    while h:
+        print(heapq.heappop(h))
